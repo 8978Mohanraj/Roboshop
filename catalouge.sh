d386d3293bfb4f79f0c343754471317760348a3f
gmr <gmrkurma234@gmail.com>
2023-04-19 22:59:00 +0530

    catalouge

--- catalouge.sh
+++ catalouge.sh
@@ -5,10 +5,12 @@ mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
-cd /app
 npm install
+cp catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
 mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
