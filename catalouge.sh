ef2f04ddabbddefb3cff79a167019216c489509c
gmr <gmrkurma234@gmail.com>
2023-04-19 17:02:22 +0530

    catalouge reposit

--- catalouge.sh
+++ catalouge.sh
@@ -1,18 +1,18 @@
-cp catalogue.repo /etc/yum.repos.d/catalouge.repo
-  echo -e "\e[36m>>>>>>>>> Copy MongoDB repo <<<<<<<<\e[0m"
+## echo -e "\e[36m>>>>>>>>> Copy MongoDB repo <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-  echo -e "\e[36m>>>>>>>>> Install nodejs <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> Install nodejs <<<<<<<<\e[0m"
 yum install nodejs -y
-  echo -e "\e[36m>>>>>>>>> add application user <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> add application user <<<<<<<<\e[0m"
 useradd roboshop
-  echo -e "\e[36m>>>>>>>>> create a new directory app <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> create a new directory app <<<<<<<<\e[0m"
+rm -rf /app
 mkdir /app
-  echo -e "\e[36m>>>>>>>>> zip the catalouge file <<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>> zip the catalouge file <<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
-cd /app
 npm install
+cp /home/centos/roboshop-shell/catalouge.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
