68aac8c9838c0b0181df78f900ea35a0f202cea9
gmr <gmrkurma234@gmail.com>
2023-04-19 22:21:53 +0530

    catalouge

--- catalouge.sh
+++ catalouge.sh
@@ -1,21 +1,14 @@
-## echo -e "\e[36m>>>>>>>>> Copy MongoDB repo <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[36m>>>>>>>>> Install nodejs <<<<<<<<\e[0m"
 yum install nodejs -y
-echo -e "\e[36m>>>>>>>>> add application user <<<<<<<<\e[0m"
 useradd roboshop
-echo -e "\e[36m>>>>>>>>> create a new directory app <<<<<<<<\e[0m"
-rm -rf /app
 mkdir /app
-echo -e "\e[36m>>>>>>>>> zip the catalouge file <<<<<<<<\e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.zip
+cd /app
 npm install
-cp /home/centos/Roboshop/catalouge.service /etc/systemd/system/catalouge.service
 systemctl daemon-reload
 systemctl enable catalogue
-systemctl restart catalogue
-cp /home/centos/Roboshop/mongo.repo /etc/yum.repos.d/mongo.repo
+systemctl start catalogue
 yum install mongodb-org-shell -y
-mongo --host mongodb-dev.gmr.dev.online </app/schema/catalogue.js
+mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
