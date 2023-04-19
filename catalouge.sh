8c182baf896bd4f7962795ae4258b060806b60d8
gmr <gmrkurma234@gmail.com>
2023-04-19 20:26:38 +0530

    catalouge

--- catalouge.sh
+++ catalouge.sh
@@ -12,7 +12,7 @@ curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue
 cd /app
 unzip /tmp/catalogue.zip
 npm install
-cp /home/centos/Roboshop /etc/systemd/system
+cp /home/centos/Roboshop/catalouge.service /etc/systemd/system/catalouge.service
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
