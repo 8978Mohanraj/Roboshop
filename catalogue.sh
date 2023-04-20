c117ea560e68179df827ab42d3f8b567193cc88c
gmr <gmrkurma234@gmail.com>
2023-04-20 10:10:24 +0530

    catalouge

--- catalogue.sh
+++ catalogue.sh
@@ -3,14 +3,14 @@ yum install nodejs -y
 useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd app
+cd /app
 unzip /tmp/catalogue.zip
 npm install
 systemctl daemon-reload
-systemctl enable catalogue.Service
-systemctl start catalogue.Service
+systemctl enable catalogue
+systemctl start catalogue
 
 yum install mongodb-org-shell -y
 mongo --host mongodb-dev.gmr.dev.online </app/schema/catalogue.js
-cp catalogue.Service /etc/systemd/system/catalogue.Service
+cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
